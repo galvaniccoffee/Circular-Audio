2c8ba565a5c99b4f24ea45fada5eaa51730a132f
galvaniccoffee <mail@jannisbecker.de>
2014-06-25 11:34:36 +0200

    Cleaned up comments
    (and a bit of code)

--- core.js
+++ core.js
@@ -2,6 +2,11 @@ var ctx, canvas, audioCtx, audio, audioSrc, bufferSrc, analyser, frequencyData, 
 lines, lineAmount, lineWidth, lineSpaceing, lineColor1, lineColor2, lineColor3, bgColor, rotation, bump, pulsation,
 screenMid, settings, col2Pckr, col3Pckr, pulseCB;
 
+
+window.onload = function () {
+	setup();
+}
+
 function setup() {
 	var i;
 
@@ -11,29 +16,24 @@ function setup() {
 	canvas.width = window.innerWidth;
 	ctx = canvas.getContext('2d');
 
-	audio = document.getElementById('player');
-	//audio.setAttribute('src', "you will be perfect.mp3");
-	//audio.setAttribute('src', "Hustle.mp3");
+	audio = document.getElementById('player'); //The HTML5 player
 	audioCtx = new (window.AudioContext || window.webkitAudioContext);
 
 	analyser = audioCtx.createAnalyser();
   	analyser.fftSize = 64;
 
 	audioSrc = audioCtx.createMediaElementSource(audio);
-	bufferSrc = audioCtx.createBufferSource();
 
 	audioSrc.connect(audioCtx.destination);
 	audioSrc.connect(analyser);
-	bufferSrc.connect(audioCtx.destination);
-	bufferSrc.connect(analyser);
 
-  	
-  	frequencyData = new Uint8Array(analyser.frequencyBinCount);
+  	frequencyData = new Uint8Array(analyser.frequencyBinCount); //The current frequency data will be stored here later.
 
+  	//Menu setup
   	settings = document.getElementById('settings');
   	settingsOpen = true;
 
-  	//Calc the Screens center point (used later)
+  	//Calc the screen's center point (used later)
 	screenMid = {x : Math.round(canvas.width / 2), y : Math.round(canvas.height / 2)};
 
 	//Settin' up data for the circles
@@ -43,18 +43,18 @@ function setup() {
 	lineColor1 = {r: 255, g: 255, b: 255, a: 0.5};
 	lineColor2 = {r: 133, g: 191, b: 37, a: 1}; //Upvote-Color!
 	lineColor3 = {r: 50, g: 191, b: 200, a: 1};
-	bgColor = "rgba(133, 191, 137, 0.3)";
+	bgColor = "rgba(133, 191, 137, 0.3)"; //For the background gradient
 	rotation = Math.PI;
-	bump = 0;
+	bump = 0; //background gradient size
 	pulsation = true;
 
-	lines = []; //Stores a number between 0 and 1 for each line. 0 = Compl. retracted; 1 = full circle
+	lines = []; //Stores a number between 0 and 1 for each line. 0 = completely retracted; 1 = full circle
 	for (i = 0; i < lineAmount; i++) {
 		//Fill with zeros
 		lines.push(0.0);
 	}
 
-	//aaaaaand start!
+	//aaaaaand go!
 	tick();
 
 	window.onresize = function(event) {
@@ -65,9 +65,11 @@ function setup() {
 		screenMid.y = Math.round(canvas.height / 2);
 	}
 
+	//Music file input setup
 	document.getElementById('fileIn').addEventListener('change', playFile, false);
 	document.body.addEventListener('drop', playFile, false);
 
+	//Options setup (color selectors etc.)
 	col2Pckr = document.getElementById('col2');
 	col3Pckr = document.getElementById('col3');
 	pulseCB = document.getElementById('pulsating');
@@ -92,42 +94,26 @@ function setup() {
 			}
 		}
 		, false);
-
-	//fileIn.addEventListener("change",
-	//	function() {
-	//		var reader = new FileReader();
-//
-	//		reader.onload = function(ev) {
-	//				audioCtx.decodeAudioData(ev.target.result, function(buffer) {
-	//					console.log("hey");
-	//					bufferSrc.buffer = buffer;
-	//					bufferSrc.start();
-	//				});
-	//		};
-//
-	//		reader.readAsArrayBuffer(this.files[0]);
-	//		console.log(this.files[0]);
-	//	}, false);
 }
 
 function tick() {
 	var i;
 	requestAnimationFrame(tick); //Repeat
-	analyser.getByteFrequencyData(frequencyData); //Frequency Data right now
+	analyser.getByteFrequencyData(frequencyData); //Frequency Data right now; integers between 0 and 256
 
 	if (pulsation) {
-		//To make the circle and background grow to the beat:
+		//To make the circle and background gradient pulsate to the beat:
 		bump = Math.pow(frequencyData[17], 1) / 256;
 		lineSpacing = (2 * lineSpacing + 5 + 2 * (((frequencyData[17] + frequencyData[27]) / 2) / 256)) / 3;
-		//lineSpacing = (2 * lineSpacing + 5 + Math.round(24 * ((Math.abs(frequencyData[28] + frequencyData[30]) / 2) / 256))) / 3;
-		//if (lineSpacing > 5) {lineSpacing = Math.round(lineSpacing / 1.3);}
 	}
 
 	//Read data to visualization...
 	for (i = 0; i < lineAmount; i++) {
-		lines[/*lineAmount - 1 - */i] = frequencyData[i] / 256;
+		lines[i] = frequencyData[i] / 256;
 	}
-	//rotation +=0.005
+
+
+	//rotation +=0.005 //Currently not used
 
 	//...and draw the circles!
 	drawLines();
@@ -136,10 +122,11 @@ function tick() {
 function drawLines() {
 	var i, grd;
 	
+	//Clean everything up
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 	if (pulsation) {
-		//Draw Background
+		//Draw background gradient
 		grd = ctx.createRadialGradient(screenMid.x, screenMid.y, 0, screenMid.x, screenMid.y, 1000 * bump);
 		grd.addColorStop(0, bgColor);
 		grd.addColorStop(1, "rgba(0, 0, 0, 0)");
@@ -163,11 +150,12 @@ function drawLines() {
 		
 
 		if (lines[i] === 0 || i === 0) {
+
 			//If the line is fully retracted, even the round caps aren't drawn, so we need to
-			//draw small circles in the specific case.
-			ctx.fillStyle = getColor(lineColor1, lineColor2, lines[i]).string;
+			//draw small circles in this specific case. Also, the center 'line' is always a dot.
+
+			ctx.fillStyle = getColor(lineColor1, lineColor2, lines[i]).string; //Color depending on line length
 			ctx.beginPath();
-			//ctx.moveTo(screenMid.x, screenMid.y);
 			ctx.arc(
 				screenMid.x - i * (lineWidth + lineSpacing),
 				screenMid.y,
@@ -175,17 +163,20 @@ function drawLines() {
 				0,
 				2 * Math.PI, false);
 			ctx.fill();
+
 		} else {
-			ctx.strokeStyle = getColor(lineColor1, getColor(lineColor2, lineColor3, i / lineAmount).col, lines[i]).string;
+
+			//In every other case simply draw the arc.
+
+			ctx.strokeStyle = getColor(lineColor1, getColor(lineColor2, lineColor3, i / lineAmount).col, lines[i]).string; //Color depending on line length
 			
 			ctx.beginPath();
-			//ctx.moveTo(screenMid.x - i * (lineWidth + lineSpacing), screenMid.y);
 			ctx.arc(
-				screenMid.x, //center
+				screenMid.x, //Center
 				screenMid.y,
-				i * (lineWidth + lineSpacing), //rad
-				rotation, //ang1
-				rotation + Math.PI * 2 * lines[i]); //ang2
+				i * (lineWidth + lineSpacing), //Radius
+				rotation, //Starting angle
+				rotation + Math.PI * 2 * lines[i]); //End Angle
 
 			ctx.stroke();
 
@@ -194,11 +185,13 @@ function drawLines() {
 }
 
 function playDemo(file) {
+	//Gets called via the button in the HTML
 	audio.setAttribute('src', file);
 	audio.play();
 }
 
 function playFile(evt) {
+	//Gets called when selecting a file
 	audio.pause();
 	evt.stopPropagation();
     evt.preventDefault();
@@ -207,30 +200,14 @@ function playFile(evt) {
     audio.play();
 }
 
-//function useMic() {
-//	audio.pause();
-//	navigator.getUserMedia = (navigator.getUserMedia ||
-//                          navigator.webkitGetUserMedia ||
-//                          navigator.mozGetUserMedia ||
-//                          navigator.msGetUserMedia);
-//	navigator.getUserMedia({audio: true}, 
-//		function(stream) {
-//			console.log(stream);
-//			microphone = audioCtx.createMediaStreamSource(stream);
-//			microphone.connect(analyser);
-//		},
-//		function(err) {
-//			console.log('The following gUM error occured: ' + err);
-//      	});
-//	
-//}
-
 function togglesettings() {
+	//Self-explaining
 	settings.style.marginBottom = "-" + settings.clientHeight * (settingsOpen ? 1 : 0) + "px";
 	settingsOpen = !settingsOpen;
 }
 
 function getColor(col1, col2, pos) {
+	//Returns the color that's a mix between the 2 given ones, with position being the ratio between them
 	var nR, nG, nB, nA;
 	nR = Math.round(col1.r * (1 - pos) + col2.r * pos);
 	nG = Math.round(col1.g * (1 - pos) + col2.g * pos);
@@ -242,6 +219,7 @@ function getColor(col1, col2, pos) {
 }
 
 function hexToRgba(hex) {
+	//Used for the color selectors
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
         r: parseInt(result[1], 16),
@@ -249,8 +227,4 @@ function hexToRgba(hex) {
         b: parseInt(result[3], 16),
         a: 1
     } : null;
-}
-
-window.onload = function () {
-	setup();
 }
